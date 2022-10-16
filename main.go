48a3ba6c4504c77d44c21077c08b8d3e37639265
sambhavjain97 <115626525+sambhavjain97@users.noreply.github.com>
2022-10-16 17:04:50 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/adityachandla/emmTrial/reader"
 	"github.com/adityachandla/emmTrial/search"
@@ -12,9 +14,20 @@ func main() {
 	houses := reader.ReadHouses()
 
 	searchRes := search.BfsEvaluate(houses)
-	fmt.Printf("Score   ScoreComplement   Difference  ScoreRelative  Conditions      Subgroup Size\n")
+
+	// Initialize tabWriter for column formatting
+	w := new(tabwriter.Writer)
+
+	// minwidth, tabwidth, padding, padchar, flags
+	w.Init(os.Stdout, 8, 8, 2, '\t', 0)
+
+	defer w.Flush()
+
+	fmt.Fprintf(w, "Score\tScoreComplement\tDifference\tScoreRelative\tConditions\tSubgroup Size\n")
+	fmt.Fprintf(w, "----\t----\t----\t----\t----\t----\n")
+
 	for searchRes.Len() > 0 {
 		node := heap.Pop(searchRes).(*search.Node)
-		fmt.Printf("%7f %7f %7f %7f %80s %5d\n", node.Score, node.ScoreComplement, node.ScoreDifference, node.ScoreRelative, node.Conditions, node.Size)
+		fmt.Fprintf(w, "%f\t%f\t%f\t%f\t%s\t%d\n", node.Score, node.ScoreComplement, node.ScoreDifference, node.ScoreRelative, node.Conditions, node.Size)
 	}
 }
